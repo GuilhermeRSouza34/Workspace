50b126a81dd3b7a9bb76509052b396448ae77e3e
GuilhermeRSouza34 <guilhermersouza34@gmail.com>
2025-08-06 10:33:38 -0300

    Criando da forma correta

--- src/ProjetosPOUI/PainelFaturamento/frontend/src/app/app-routing.module.ts
+++ src/ProjetosPOUI/PainelFaturamento/frontend/src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PedidosPendentesComponent } from './components/pedidos-pendentes/pedidos-pendentes.component';
-import { RankingClientesComponent } from './components/ranking-clientes/ranking-clientes.component';
-import { RelatoriosComponent } from './components/relatorios/relatorios.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'pedidos-pendentes', component: PedidosPendentesComponent },
-  { path: 'ranking-clientes', component: RankingClientesComponent },
-  { path: 'relatorios', component: RelatoriosComponent },
   { path: '**', redirectTo: '/dashboard' }
 ];
 
